a4198f34ae44c7395de0d3fcdf93e9f423e76923
skeshavaa <s.keshavaa@gmail.com>
2021-03-23 22:54:05 -0400

    more starter code

--- final.asm
+++ final.asm
@@ -15,4 +15,13 @@
 	sw $t2, 4($t0) # paint the second unit on the first row green. Why $t0+4?
 	sw $t3, 128($t0) # paint the first unit on the second row blue. Why +128?
 	li $v0, 10 # terminate the program gracefully
-
+	
+main:
+	li $t9, 0xffff0000 
+	lw $t8, 0($t9)
+	beq $t8, 1, keypress_happened
+	
+	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000from before
+	beq $t2, 0x61, respond_to_a # ASCII code of 'a' is 0x61 or 97 in decimal
+	
+	syscall
