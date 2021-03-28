01c4e1704005834860e21a94fa6618f3ca3f3b58
skeshavaa <s.keshavaa@gmail.com>
2021-03-28 00:51:43 -0400

    randomly generating one block

--- final.asm
+++ final.asm
@@ -6,17 +6,20 @@
 # -Base Address for Display: 0x10008000 ($gp)
 #
 .eqv BASE_ADDRESS0x10008000
+
+.data
+	c1:    .word    1, 10, 20
+	newline: .asciiz "\n"
+
 .text
+	la $t4, c1
+
 	li $t0, 0x10008000 # $t0 stores the base address for display
 	li $t1, 0xff0000 # $t1 stores the red colour 
 	li $t2, 0x808080 # $t2 stores the green colour code 
 	li $t3, 0x0000ff # $t3 stores the blue colour code
 	li $v0, 10 # terminate the program gracefully
 	
-	sw $t2, 0($t0)
-	sw $t2, 4($t0)
-	sw $t2, 8($t0)
-	sw $t1, 12($t0)
 	
 	sw $t2, 260($t0)
 	sw $t2, 264($t0)
@@ -34,11 +37,7 @@
 	
 	sw $t2, 1284($t0)
 	sw $t2, 1288($t0)
-	
-	sw $t2, 1536($t0)
-	sw $t2 1540($t0)
-	sw $t2 1544($t0)
-	sw $t1, 1548($t0)
+
 	
 main:
 	li $t9, 0xffff0000 
@@ -47,5 +46,23 @@ main:
 	
 	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000from before
 	#beq $t2, 0x61, respond_to_a # ASCII code of 'a' is 0x61 or 97 in decimal
-	jal main
+	jal draw_obstacles
+	li $v0, 10
 	syscall
+
+draw_obstacles:
+	li $a1, 31  #Here you set $a1 to the max bound.
+    	li $v0, 42  #generates the random number.
+    	syscall
+    	li $v0, 1   # 1 is the system call code to show an int number
+	syscall     # as I said your generated number is at $a0, so it will be printed
+	move $t5, $a0	
+	li $v0 4
+	la $a0 newline
+	syscall
+	
+	mul $t5, $t5, 256
+	addi $t5, $t5, 252
+	add $t0, $t5, $t0
+    	sw $t3, 0($t0)
+	jr $ra
