8c33c0b6b0955b74479755cff195f196d22f580d
skeshavaa <s.keshavaa@gmail.com>
2021-03-28 15:23:16 -0400

    successfully branching when c1 is drawn

--- final.asm
+++ final.asm
@@ -8,8 +8,10 @@
 .eqv BASE_ADDRESS0x10008000
 
 .data
-	c1:    .word    1, 10, 20
+	c1:    .word    1, 0, 69
 	newline: .asciiz "\n"
+	special: .asciiz "hi!"
+	offset: .byte 4
 
 .text
 	la $t4, c1
@@ -40,29 +42,58 @@
 
 	
 main:
-	li $t9, 0xffff0000 
-	lw $t8, 0($t9)
+	#li $t9, 0xffff0000 
+	#lw $t8, 0($t9)
 	#beq $t8, 1, keypress_happened
 	
-	lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000from before
+	#lw $t2, 4($t9) # this assumes $t9 is set to 0xfff0000from before
 	#beq $t2, 0x61, respond_to_a # ASCII code of 'a' is 0x61 or 97 in decimal
-	jal draw_obstacles
+	li $t0, 0x10008000 # $t0 stores the base address for display
+	la $t4, c1
+	
+	lw $t4, 4($t4)
+	beq $t4, $zero, draw_obstacles
+	j move_obstacles
+	
+loop:	
+	li $v0, 32
+	li $a0, 500 #Sleep for 500 seconds
+	syscall
+	
+	j main
 	li $v0, 10
 	syscall
 
 draw_obstacles:
 	li $a1, 31  #Here you set $a1 to the max bound.
     	li $v0, 42  #generates the random number.
+    	la $t4, c1
     	syscall
-    	li $v0, 1   # 1 is the system call code to show an int number
-	syscall     # as I said your generated number is at $a0, so it will be printed
+    	
+    	
+	
 	move $t5, $a0	
-	li $v0 4
-	la $a0 newline
-	syscall
 	
 	mul $t5, $t5, 256
 	addi $t5, $t5, 252
 	add $t0, $t5, $t0
     	sw $t3, 0($t0)
-	jr $ra
+    	sw $t0, 0($t4)
+    	
+    	li $v0, 1
+    	move $a0, $t0
+	syscall
+	li $v0 4
+	la $a0 newline
+	syscall
+	
+    	addi $t3, $zero, 1
+    	sw $t3, 4($t4)
+    	
+	j loop
+
+move_obstacles:
+	li $v0 4
+	la $a0 special
+	syscall
+	j loop
